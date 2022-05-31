2b880dbdd2946c213e6e48e418d51437971ff295
Pithayuth Charnsethikul <pcharnset@piranha-agenda.isi.edu>
2022-05-31 16:02:04 -0400

    remove setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-pip cache purge
-pip install -U scikit-learn
-pip install -r requirements.txt
-pip install snips-nlu
-python -m snips_nlu download en
-pip install spacy
-python -m spacy download en_core_web_sm
-python -m spacy download en_core_web_lg
