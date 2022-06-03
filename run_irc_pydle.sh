7212ee0afb71cef71a64f6add634d180de879921
charnset <charnset@usc.edu>
2022-06-03 13:47:01 -0700

    adjust pydle bot accordingly to new puppeteer interface, in particular new interface for additional NLI trigger detectors

--- run_irc_pydle.sh
+++ run_irc_pydle.sh
@@ -1,8 +1,7 @@
 export SERVER=10.101.110.39
 export PORT=6697
 export NUM_BOTS=3
-export AGENDA_DIR=puppeteer-control/agendas/
-export NLU_DATA_DIR=puppeteer-control/nlu_training_data/
+export AGENDA_DIR=puppeteer-control
 
 python3 irc_pydle.py \
     --server $SERVER \
@@ -10,4 +9,3 @@ python3 irc_pydle.py \
     --num_bots $NUM_BOTS \
     --agenda_dir $AGENDA_DIR \
     --agenda_names get_shipment get_payment get_website\
-    --nlu_data_dir $NLU_DATA_DIR
